31f672bcc2d2b888e3b5b0a4cbff8b933a07b1ca
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-03-28 23:43:43 +0800

    Add GitRepository.GetRootPath method.

---
+++ Mile.Project.Helpers/GitRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace Mile.Project.Helpers
+{
+    public class GitRepository
+    {
+        public static string GetRootPath()
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    FileName = "git.exe",
+                    Arguments = "rev-parse --show-toplevel"
+                }
+            };
+
+            if (process.Start())
+            {
+                process.WaitForExit();
+                if (process.ExitCode == 0)
+                {
+                    string? result = process.StandardOutput.ReadLine();
+                    if (result != null)
+                    {
+                        return Path.GetFullPath(result);
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
