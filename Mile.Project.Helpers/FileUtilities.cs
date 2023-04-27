6e768d397ae8d302ebffa4e936763fee6f9b81b5
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-04-27 21:53:52 +0800

    Add FileUtilities.ConvertFileEncoding method.

---
+++ Mile.Project.Helpers/FileUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Mile.Project.Helpers
+{
+    public class FileUtilities
+    {
+        public static void ConvertFileEncoding(
+            string FilePath,
+            Encoding InputEncoding,
+            Encoding OutputEncoding)
+        {
+            File.WriteAllText(
+                FilePath,
+                File.ReadAllText(FilePath, InputEncoding),
+                OutputEncoding);
+        }
+    }
+}
